b9c08634f223e67aa909f91ac7a304167b97350c
kkt9253 <kkt9253@naver.com>
2025-12-26 14:59:28 +0900

    feat: test api 추가 및 프로젝트 port 변경

---
+++ src/test/java/githubactionpractice/githubactionpractice/test/TestControllerTest.java
@@ -0,0 +1,20 @@
+package githubactionpractice.githubactionpractice.test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+class TestControllerTest {
+
+    @Test
+    void health_returns_OK() {
+        TestController c = new TestController();
+        assertThat(c.testMethod()).isEqualTo("Hi! This is GithubAction test api request! success!!");
+    }
+
+//    @Test
+//    void health_returns_Fail() {
+//        TestController c = new TestController();
+//        assertThat(c.testMethod()).isEqualTo("Fail");
+//    }
+}
